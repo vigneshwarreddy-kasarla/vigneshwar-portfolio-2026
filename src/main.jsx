18f6456fc7a90f81e08fec2b69e7453e80fff324
vigneshwar reddy kasarla <vigneshwarreddykasarla6@gmail.com>
2025-12-06 20:49:43 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "./style.css";   // ✅ This file WILL exist now
+import "./index.css";   // ✅ CORRECT FILE
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
